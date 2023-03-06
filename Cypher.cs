a4af86b9d3fcc38d5334dd34e1712c6924f200ca
tdk-cm <cme@td-k.dk>
2023-03-06 12:19:19 +0100

    Add project files.

---
+++ Cypher.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Krypto1
+{
+    internal class Cypher
+    {
+        static int amountToShift = 1;
+        public static string EncryptString(string input)
+        {
+            string output = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char c = input[i];
+                c += (char)1;
+                output += c;
+            }
+
+            return output;
+        }
+
+        public static string DecryptString(string input)
+        {
+            string output = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char c = input[i];
+                c -= (char)1;
+                output += c;
+            }
+
+            return output;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using ClosedXML.Excel;
+using System.Diagnostics;
+using System.Security.Cryptography;
+
+namespace Krypto1
+{
+    internal class Program
+    {
+        static int iterations = 10000;
+        static void Main(string[] args)
+        {
+            //StartTests();
+            CypherUp();
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+
+        static void CypherUp()
+        {
+            string initial = "Hello from California";
+            Console.WriteLine("Cypher!");
+            Console.WriteLine($"Initial string: {initial}");
+
+            string encrypted = Cypher.EncryptString(initial);
+            Console.WriteLine($"Encrypted: {encrypted}");
+
+            string decrypted = Cypher.DecryptString(encrypted);
+            Console.WriteLine($"Decrypted: {decrypted}");
+        }
+
+        static void StartTests()
+        {
+            Stopwatch randomWatch = Stopwatch.StartNew();
+            List<int> randomList = TestRandom();
+            randomWatch.Stop();
+
+            Stopwatch cryptoWatch = Stopwatch.StartNew();
+            List<int> cryptoList = TestRngProvider();
+            cryptoWatch.Stop();
+
+
+            var wb = new XLWorkbook();
+            var ws = wb.Worksheets.Add("cryptoTestData");
+            ws.Cell(1, 1).SetValue("System.Random");
+            ws.Cell(2, 1).SetValue($"{iterations.ToString()} iterations");
+            ws.Cell(3, 1).SetValue($"Time elapsed: {randomWatch.ElapsedMilliseconds} ms");
+            ws.Cell(1, 2).InsertData(randomList);
+
+            ws.Cell(1, 3).SetValue("RNGCryptoServiceProvider");
+            ws.Cell(2, 3).SetValue($"{iterations.ToString()} iterations");
+            ws.Cell(3, 3).SetValue($"Time elapsed: {cryptoWatch.ElapsedMilliseconds} ms");
+            ws.Cell(1, 4).InsertData(cryptoList);
+            wb.SaveAs(@"C:\Skole\Crypto\testData.xlsx");
+        }
+
+        static List<int> TestRandom()
+        {
+            List<int> returnList = new List<int>();
+
+            Random rand = new Random();
+
+            for (int i = 0; i < iterations; i++)
+            {
+                returnList.Add(rand.Next());
+            }
+
+            return returnList;
+        }
+
+        static List<int> TestRngProvider()
+        {
+            List<int> returnList = new List<int>();
+
+            using (RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider())
+            {
+                byte[] number = new byte[4];
+
+                for (int i = 0; i < iterations; i++)
+                {
+                    provider.GetBytes(number);
+
+                    returnList.Add(BitConverter.ToInt32(number));
+                }
+            }
+
+            return returnList;
+        }
+    }
+}
